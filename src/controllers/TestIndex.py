7f898ff66ea06e85fe536d93c7a9ba524b1f1d37
Aurel <shiftsh@pm.me>
2019-10-02 13:43:13 +0100

    tests: add test for IndexCOntroller functions and for Application Controllers function

--- src/controllers/TestIndex.py
+++ src/controllers/TestIndex.py
@@ -1,15 +1,16 @@
 from unittest import TestCase
+from src import app
 
 
-# from tests import app
-
-
-class IndexController(TestCase):
+class TestIndex(TestCase):
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
-    def test_create_supervisor_config(self):
-        pass
+    def test_index(self):
+        # test login page on non connected user where I expect a redirection
+        with app.test_client() as c:
+            rv = c.get('/', follow_redirects=False)
+            self.assertEqual(rv.status_code, 302)
---
+++ tests/__init__.py
@@ -0,0 +1,13 @@
+import sys
+
+import trace
+import unittest
+from unittest import TestSuite
+
+sys.path.append(".")
+sys.path.append("../")
+
+# from ftpserver.TestAppAuthorizer import TestAppAuthorizer
+from src.controllers.TestIndex import TestIndex
+from src.controllers.TestApplication import TestApplication
+
