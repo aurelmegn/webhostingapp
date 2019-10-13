98c2b2eb64307dd1ea3cacf10d7c6dee83157565
Aurel <shiftsh@pm.me>
2019-10-13 20:39:44 +0100

    fix: the logout session which in the old version remain on the user side

--- src/controllers/index.py
+++ src/controllers/index.py
@@ -1,7 +1,17 @@
+from flask_security import logout_user
+
 from src import app
-from flask import render_template
+from flask import render_template, session, redirect
 
 
 @app.route("/")
 def index():
     return render_template("default/index.jinja")
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    session.clear()
+
+    return redirect("/")
